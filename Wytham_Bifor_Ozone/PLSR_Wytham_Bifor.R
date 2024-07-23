73d9e5b97acf360423501aae672ea248c41950ab
anna-jones-ecology <75208283+anna-jones-ecology@users.noreply.github.com>
2024-07-23 18:13:20 +0100

    PLSR test vs train

--- Wytham_Bifor_Ozone/PLSR_Wytham_Bifor.R
+++ Wytham_Bifor_Ozone/PLSR_Wytham_Bifor.R
@@ -14,39 +14,57 @@ setwd("~/Library/CloudStorage/OneDrive-Nexus365/01 OXFORD PHD/Field Work 2023/R_
 load(file = "~/Library/CloudStorage/OneDrive-Nexus365/01 OXFORD PHD/Field Work 2023/R_Data_Analysis/QWYTHAM_BIFOR_merged.RData")
 head(merged)
 merged<-subset(merged,merged$ozone!="NA")
+
+#average per site/date
+merged$site_date<-paste(merged$site, merged$date)
+site_date_means<-aggregate(merged, merged$site_date, mean)
+head(site_date_means)
+
+#split the dataset by site/date
+merged$site_date<-as.factor(merged$site_date)
+stdts<-levels((merged$site_date))
+lst<-list()
+var_names<-list()
+v<-new("hyperSpec")
+train_merged<-new("hyperSpec")
+test_merged<-new("hyperSpec")
+#make a list of subsetted hyperspectral objects
+for (n in 1:length(stdts)){
+  var_names<-append(var_names, paste(stdts[n],"train", sep="_"))
+  var_names<-append(var_names, paste(stdts[n],"test", sep="_"))
+  v<-subset(merged, merged$site_date==stdts[n])
+  sample_set<- sample(c(TRUE, FALSE), nrow(v), replace=TRUE, prob=c(0.7,0.3))
+  train<-v[sample_set,]
+  test<-v[!sample_set,]
+  train_merged<-collapse(train_merged,train)
+  test_merged<-collapse(test_merged,test)
+}
+
+#now average the test_merged and train_merged by site/date
+train_means<-aggregate(train_merged, train_merged$site_date, mean)
+test_means<-aggregate(test_merged, test_merged$site_date, mean)
+
 #fit the  partial least squares model
 #make this example reproducible
 set.seed(1)
 
-#fit PLSR model
-model <- plsr(ozone~spc, data=merged, scale=TRUE, validation="CV")#left running for a while, started 13:00 ish, finished 16:30, mac may have gone to sleep in between 
+#fit PLSR model based on the train data set
+model <- plsr(ozone~spc, data=train_means, scale=TRUE, validation="CV")
 summary(model)
 #visualise cross validation plots
-validationplot(model, ncomp=1:60)#Root mean square deviation, in units of predictor, more useful
+validationplot(model)#Root mean square deviation, in units of predictor, more useful
 validationplot(model, val.type="MSEP")#mean squared prediction error, not in same units as predictor
-validationplot(model, val.type="R2", ncomp=1:60)
-#optimal number of comps looks to be around 35
+#optimal number of comps looks to be 2
+scoreplot(model)
+loadingplot(model,comps=1:2, legendpos = "topright", labels="numbers", xlab="Wavelength (nm)")
 
-
-
-#now split the dataset into test and train and fit for 60 components, 80:20 split
-sample <- sample(c(TRUE, FALSE), nrow(merged), replace=TRUE, prob=c(0.8,0.2))
-train<-merged[sample,]
-test<-merged$spc[!sample,]
-y_test<-merged$ozone[!sample]
-
-#make the model based on train set
-model_2<-plsr(ozone~spc, data=train, scale=TRUE, validation="CV",ncomp=60)#runs fast
-summary(model_2)
-validationplot(model_2)#Root mean square deviation, in units of predictor, minima at 35 comps
-validationplot(model_2, val.type="R2")
 #make predictions on test set
-pcr_pred <- predict(model_2, test, ncomp=35)
+test<-test_means$spc
+y_test<-test_means$ozone
+pcr_pred <- predict(model, test, ncomp=2)
 plot(y_test, pcr_pred, main="Test Dataset", xlab="Observed", ylab="PLS Predicted")
 abline(0,1,col="red")
 #evaluate the plsr model's performance for test data
 pls_eval<-data.frame(obs=y_test, pred=pcr_pred[,1,1])
 defaultSummary(pls_eval)
-#output:
-# RMSE  Rsquared       MeanAbsoluteError 
-# 4.5789956 0.6565933 3.1594922 
+
--- Wytham_Bifor_Ozone/VIs_Wytham_Bifor.R
+++ Wytham_Bifor_Ozone/VIs_Wytham_Bifor.R
@@ -2,6 +2,7 @@
 #load packages
 library(hyperSpec)
 library(ggplot2)
+library(stats)
 #set working directory
 setwd("~/Library/CloudStorage/OneDrive-Nexus365/01 OXFORD PHD/Field Work 2023/R_Data_Analysis")
 #load merged hyperspectral library (QWytham and BiFOR)
@@ -13,15 +14,32 @@ merged$NDVI<-as.numeric(((merged[,,800]$spc)-(merged[,,670]$spc))/((merged[,,800
 merged$MCARI<-as.numeric((((merged[,,700]$spc)-(merged[,,670]$spc))-0.2*((merged[,,700]$spc)-(merged[,,550]$spc)))*((merged[,,700]$spc)/(merged[,,670]$spc)))
 merged$PRI<-as.numeric(((merged[,,570]$spc)-(merged[,,530]$spc))/((merged[,,570]$spc)+(merged[,,530]$spc)))
 merged$PSRI<-as.numeric(((merged[,,680]$spc)-(merged[,,500]$spc))/((merged[,,680]$spc)+(merged[,,500]$spc)))
+merged$site_date<-paste(merged$site, merged$date)
 
-VIdf<-data.frame(merged$site ,merged$ozone,merged$OZDI,merged$NDVI,merged$MCARI, merged$PRI, merged$PSRI)#needed for ggplots
-colnames(VIdf)<-c("site","ozone","OZDI","NDVI","MCARI","PRI","PSRI")
-as.factor(VIdf$site)
+VIdf<-data.frame(merged$site, merged$date, merged$site_date ,merged$ozone,merged$OZDI,merged$NDVI,merged$MCARI, merged$PRI, merged$PSRI)#needed for ggplots
+colnames(VIdf)<-c("site", "date", "site_date","ozone","OZDI","NDVI","MCARI","PRI","PSRI")
 
-ggplot(VIdf, aes(x=ozone,y=MCARI,color=site))+geom_point()+scale_color_manual(values=c("red","blue"))
-ggplot(VIdf, aes(x=site,y=MCARI,color=site))+geom_boxplot()+scale_color_manual(values=c("red","blue"))
-ggplot(VIdf, aes(x=site,y=OZDI,color=site))+geom_boxplot()+scale_color_manual(values=c("red","blue"))
-ggplot(VIdf, aes(x=site,y=NDVI,color=site))+geom_boxplot()+scale_color_manual(values=c("red","blue"))
-ggplot(VIdf, aes(x=site,y=PRI,color=site))+geom_boxplot()+scale_color_manual(values=c("red","blue"))
-ggplot(VIdf, aes(x=site,y=PSRI,color=site))+geom_boxplot()+scale_color_manual(values=c("red","blue"))
-#MCARI SHOWS QUITE GOOD DIFFRENTIATION BETWEEN SITES
+
+VIdf<-as.data.frame.list(VIdf)
+means_VI<-aggregate(VIdf, list(VIdf$site_date, VIdf$site),mean)
+means_VI<-means_VI[-5]
+means_VI<-means_VI[-3]
+colnames(means_VI)<-c("site_date", "site", "date",  "ozone", "OZDI" ,"NDVI", "MCARI", "PRI", "PSRI" )
+
+ggplot(means_VI, aes(x=ozone,y=MCARI,color=site))+geom_point()+scale_color_manual(values=c("red","blue"))
+ggplot(means_VI, aes(x=site,y=MCARI,color=site))+geom_boxplot()+scale_color_manual(values=c("red","blue"))
+
+ggplot(means_VI, aes(x=ozone,y=OZDI,color=site))+geom_point()+scale_color_manual(values=c("red","blue"))
+ggplot(means_VI, aes(x=site,y=OZDI,color=site))+geom_boxplot()+scale_color_manual(values=c("red","blue"))
+
+ggplot(means_VI, aes(x=ozone,y=NDVI,color=site))+geom_point()+scale_color_manual(values=c("red","blue"))
+ggplot(means_VI, aes(x=site,y=NDVI,color=site))+geom_boxplot()+scale_color_manual(values=c("red","blue"))
+
+ggplot(means_VI, aes(x=ozone,y=PRI,color=site))+geom_point()+scale_color_manual(values=c("red","blue"))
+ggplot(means_VI, aes(x=site,y=PRI,color=site))+geom_boxplot()+scale_color_manual(values=c("red","blue"))
+
+ggplot(means_VI, aes(x=ozone,y=PSRI,color=site))+geom_point()+scale_color_manual(values=c("red","blue"))
+ggplot(means_VI, aes(x=site,y=PSRI,color=site))+geom_boxplot()+scale_color_manual(values=c("red","blue"))
+#MCARI and NDVI SHOWS QUITE GOOD DIFFRENTIATION BETWEEN SITES and correlation to ozone
+
+#correlative test of MCARI to ozone concentration 
